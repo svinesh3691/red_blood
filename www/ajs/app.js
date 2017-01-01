948c77a3cd72c01388d4a24b63ba7ebc5f233d64
S Vinesh <svinesh3691@gmail.com>
2017-01-01 17:19:06 +0530

    Bla

--- www/ajs/app.js
+++ www/ajs/app.js
@@ -17,11 +17,11 @@ function onLoad() {
 // onDeviceReady
 function onDeviceReady() {
 		// Back Button Management
-        // document.addEventListener("backbutton", function (e) {
-        //     e.preventDefault();
-        //     var exit_confirm = confirm('Are you sure to exit app? ');
-        //     if(exit_confirm) navigator.app.exitApp();
-        // }, false );
+        document.addEventListener("backbutton", function (e) {
+            e.preventDefault();
+            var exit_confirm = confirm('Are you sure to exit app? ');
+            if(exit_confirm) navigator.app.exitApp();
+        }, false );
 
 
 }
--- www/ajs/controllers/home.controller.js
+++ www/ajs/controllers/home.controller.js
@@ -3,10 +3,10 @@ app.controller('home', ['$scope','seven','$state','services',
     function ( $scope, seven, $state, services ) {
 
             seven.showIndicator();
+			if(localStorage.uthir_user) {
+					$scope.data = JSON.parse(localStorage.uthir_user);
+			}
 			
-			$scope.data = JSON.parse(localStorage.uthir_user);
-
-            var self = JSON.parse(localStorage.uthir_user);
 
             services.master('uthiram/donors_search',{}).then(function(res){
                 $scope.donors =  res.data.donors;
